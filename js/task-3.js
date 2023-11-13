d8f53cfa9e646aa31d92470846d8b2a0f4d5b66d
13Master13 <141366261+13Master13@users.noreply.github.com>
2023-11-13 18:59:44 +0200

    Settings

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-function filterArray(numbers, value) {
-	const clearnArray = []
-	for (let i = 0; i < numbers.length; i++) {
-		if (numbers[i] > value) {
-			clearnArray.push(numbers[i])
-		}
-	}
-	return clearnArray
-}
